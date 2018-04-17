09d5b737c6ed51337fe218d030689210b12c4a5e
miamastro <35510979+miamastro@users.noreply.github.com>
2018-04-17 18:14:35 -0500

    added microsite with www

---
+++ microsite/www/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$(".container").click(function(){
+		$(this).toggleClass("change");
+		$("#drawer").toggleClass("open");
+		// $("a#Career").toggleClass("display");
+
+	});
+	
+
+});
+
